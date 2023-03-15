4ff34c1e876794605af1e69201674d8edbcc92ee
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-15 14:50:02 +0100

    Updating the documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    
+    screens: {
+      sm: '480px',
+      md: '760px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
